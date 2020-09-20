cddacc0c5b337b152b2611fe644daffbc5aa5353
Alex <sivkov78@ya.ru>
2020-09-20 22:19:23 +0400

    fixed table data table events

--- src/index.js
+++ src/index.js
@@ -6,24 +6,24 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 
-const initilaState  = [
-  {'id': 1,'operator':'1','territory':'1', 'value1':'100',  'value2':'20',   'value3':'1111'},
-  {'id': 2,'operator':'1','territory':'2', 'value1':'100',  'value2':'20',   'value3':'111'},
-  {'id': 3,'operator':'1','territory':'3', 'value1':'100',  'value2':'20',   'value3':'111'},
-  {'id': 4,'operator':'2','territory':'1', 'value1':'100',  'value2':'20',   'value3':'11111'},
-  {'id':5,'operator':'3','territory':'1', 'value1':'100',  'value2':'20',   'value3':'1111'},
-  {'id':6 ,'operator':'4','territory':'1', 'value1':'100',  'value2':'20',   'value3':'11'},
-  {'id':7,'operator':'1','territory':'1', 'value1':'100',  'value2':'20',   'value3':'1111'},
-  {'id':8,'operator':'1','territory':'2', 'value1':'100',  'value2':'20',   'value3':'111'},
-  {'id':9,'operator':'1','territory':'3', 'value1':'100',  'value2':'20',   'value3':'111'},
-  {'id':10,'operator':'2','territory':'1', 'value1':'100',  'value2':'20',   'value3':'11111'},
-  {'id':11,'operator':'3','territory':'1', 'value1':'100',  'value2':'20',   'value3':'1111'},
-  {'id': 12,'operator':'4','territory':'1', 'value1':'100',  'value2':'20',   'value3':'11'},
-  {'id': 13,'operator':'1','territory':'1', 'value1':'100',  'value2':'20',   'value3':'1111'},
-  {'id': 14,'operator':'1','territory':'3', 'value1':'100',  'value2':'20',   'value3':'111'},
-  {'id': 15,'operator':'2','territory':'1', 'value1':'100',  'value2':'20',   'value3':'11111'},
-  {'id': 17,'operator':'3','territory':'1', 'value1':'100',  'value2':'20',   'value3':'1111'},
-  {'id': 18,'operator':'4','territory':'1', 'value1':'100',  'value2':'20',   'value3':'11'},
+const initilaState = [
+  { 'id': 1,  'operator': 'operator1',  'territory': 1, 'value1': 101, 'value2': 201, 'value3': '1111' },
+  { 'id': 2,  'operator': 'operator1',  'territory': 2, 'value1': 102, 'value2': 202, 'value3': '111' },
+  { 'id': 3,  'operator': 'operator1',  'territory': 3, 'value1': 102, 'value2': 203, 'value3': '111' },
+  { 'id': 4,  'operator': 'operator2',  'territory': 1, 'value1': 105, 'value2': 201, 'value3': '11111' },
+  { 'id': 5,  'operator': 'operator3',  'territory': 1, 'value1': 100, 'value2': 202, 'value3': '1111' },
+  { 'id': 6,  'operator': 'operator4',  'territory': 1, 'value1': 110, 'value2': 203, 'value3': '11' },
+  { 'id': 7,  'operator': 'operator5',  'territory': 1, 'value1': 100, 'value2': 201, 'value3': '1111' },
+  { 'id': 8,  'operator': 'operator6',  'territory': 2, 'value1': 110, 'value2': 202, 'value3': '111' },
+  { 'id': 9,  'operator': 'operator7',  'territory': 3, 'value1': 100, 'value2': 203, 'value3': '111' },
+  { 'id': 10, 'operator': 'operator8',  'territory': 1, 'value1': 100, 'value2': 201, 'value3': '11111' },
+  { 'id': 11, 'operator': 'operator9',  'territory': 1, 'value1': 100, 'value2': 202, 'value3': '1111' },
+  { 'id': 12, 'operator': 'operator10', 'territory': 1, 'value1': 100, 'value2': 203, 'value3': '11' },
+  { 'id': 13, 'operator': 'operator11', 'territory': 1, 'value1': 100, 'value2': 201, 'value3': '1111' },
+  { 'id': 14, 'operator': 'operator12', 'territory': 3, 'value1': 100, 'value2': 202, 'value3': '111' },
+  { 'id': 15, 'operator': 'operator13', 'territory': 1, 'value1': 100, 'value2': 203, 'value3': '11111' },
+  { 'id': 17, 'operator': 'operator14', 'territory': 1, 'value1': 100, 'value2': 201, 'value3': '1111' },
+  { 'id': 18, 'operator': 'operator15', 'territory': 1, 'value1': 100, 'value2': 203, 'value3': '11' },
 ];
 
 
--- src/resources/components/table.js
+++ src/resources/components/table.js
@@ -4,68 +4,91 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 
 
-
-const columns = [
-  {
-    dataField: 'id',
-    text: '#',   
-    sort: true,
-    footer:"",
-    sort: true, 
-   
-  },{
-  dataField: 'operator',
-  text: 'operator',   
-  sort: true,
-  footer: '',
-  sort: true, 
-}, {
-  dataField: 'territory',
-  text: 'territory',
-  sort: true,    
-  footer: columnData => columnData.reduce((acc, item) => acc + Number(item), 0)
-}, {
-  dataField: 'value1',
-  text: 'value1', 
-  sort: true,  
-  footer: columnData => columnData.reduce((acc, item) => acc + Number(item), 0)
-}, {
-  dataField: 'value2',
-  text: 'value2', 
-  sort: true,  
-  footer: columnData => columnData.reduce((acc, item) => acc + Number(item), 0)
-}, {
-  dataField: 'value3',
-  text: 'value3',  
-  sort: true,  
-  footer: columnData => columnData.reduce((acc, item) => acc + Number(item), 0)
-}];
-
 class TableBasic extends React.Component {
   constructor(props) {
     super(props);
     this.products = this.props.data
+    this.current = false
+    this.columns = [
+      {
+        dataField: 'id',
+        text: '#',
+        sort: true,
+        footer: "",
+        sort: true,
 
-    }
-  
+      }, {
+        dataField: 'operator',
+        text: 'operator',
+        sort: true,
+        footer: '',
+        sort: true,
+      }, {
+        dataField: 'territory',
+        text: 'territory',
+        sort: true,
+        footer: columnData => columnData.reduce((acc, item) => acc + Number(item), 0)
+      }, {
+        dataField: 'value1',
+        text: 'value1',
+        sort: true,
+        footer: columnData => columnData.reduce((acc, item) => acc + Number(item), 0),
+        headerEvents: {
+          onClick: (e, column, columnIndex) => {
+            this.current = columnIndex;
+            console.log(this.current)
+            
+          }
+        }
+
+      }, {
+        dataField: 'value2',
+        text: 'value2',
+        sort: true,
+        footer: columnData => columnData.reduce((acc, item) => acc + Number(item), 0),
+        headerEvents: {
+          onClick: (e, column, columnIndex) => {
+            this.current = columnIndex
+            console.log(this.current)
+          }
+        }
+      }, {
+        dataField: 'value3',
+        text: 'value3',
+        sort: true,
+        footer: columnData => columnData.reduce((acc, item) => acc + Number(item), 0),
+        headerEvents: {
+          onClick: (e, column, columnIndex) => {
+            this.current = columnIndex
+            console.log(this.current)
+          }
+        }
+      }];
+  }
+
+  componentDidMount() {
+    this.columns.forEach((id, index) => {
+      /*  */
+    })
+  }
+
   render() {
     return (
       <div>
         <BootstrapTable
           keyField="id"
-          data={  this.products }
-          columns={ columns }
+          data={this.products}
+          columns={this.columns}
           bootstrap4
-          pagination={ paginationFactory() }
+          pagination={paginationFactory()}
           striped
           hover
-        
+
         />
       </div>
     );
   }
 }
-
 
 export default connect(
   state => ({
